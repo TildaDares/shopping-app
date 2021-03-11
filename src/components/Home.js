3a277bd5688fff0389f2b795382d78836fee6103
Tilda Udufo <mathildaudufo@gmail.com>
2021-03-11 01:11:25 +0100

    Added a fewthings to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,36 @@
 import React from "react";
+import cart from "../images/cart.svg";
 import mainImage from "../images/gilmore-collection.jpg";
 
 export default function Home() {
 	const styles = {
-		backgroundImage: "url(" + mainImage + ")",
 		backgroundColor: "rgba(0, 0, 0, 0.5)",
-    borderLeft: "10px solid red",
+		borderLeft: "10px solid red",
+		filter: "rgba(0, 0, 0, 0.5)",
 	};
 
 	return (
 		<div
-			style={{ backgroundImage: styles.backgroundImage }}
+			style={{ backgroundImage: "url(" + mainImage + ")" }}
 			className="h-screen w-full bg-no-repeat bg-cover"
 		>
+			<nav
+				className="list-none text-white text-xl md:text-3xl flex justify-end pt-4"
+				style={{ backgroundColor: styles.filter }}
+			>
+				<li className="inline p-3 hover:bg-gray-700 md:mr-5 rounded active:text-red-600 cursor-pointer">
+					Home
+				</li>
+				<li className="inline p-3 md:mr-5 rounded hover:bg-gray-700 cursor-pointer">
+					Shop
+				</li>
+				<li className="inline p-3 pr-5 md:mr-64 rounded hover:bg-gray-700 cursor-pointer">
+					<img src={cart} alt="cart" className="w-10 inline md:w-12" />
+				</li>
+			</nav>
+
 			<div
-				className="flex justify-center items-center h-screen px-4 flex-col"
+				className="flex justify-center items-center h-screen w-screen px-4 flex-col"
 				style={{ backgroundColor: styles.backgroundColor }}
 			>
 				<p
@@ -23,7 +39,9 @@ export default function Home() {
 				>
 					<span className="text-red-600 mr-3">Phoenix </span> Jewellery
 				</p>
-        <button className="text-center bg-white text-black p-3 font-serif rounded mt-6 hover:bg-gray-200 lg:text-lg">Shop Now</button>
+				<button className="text-center bg-white text-black p-3 font-serif rounded mt-6 hover:bg-gray-200 lg:text-lg">
+					Shop Now
+				</button>
 			</div>
 		</div>
 	);
