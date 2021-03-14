f45d6d479b896171d4297aa67b8d8befe2e1e6a6
TildaDares <mathildaudufo@gmail.com>
2021-03-14 22:43:17 +0400

    edited home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
-import Home from "./components/Home";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React from "react";
+import cart from "./images/cart.svg";
+import mainImage from "./images/gilmore-collection.jpg";
 
 export default function App() {
-	return (
-		<div>
-			<BrowserRouter>
-				<Switch>
-					<Route exact path="/" component={Home} />
-				</Switch>
-			</BrowserRouter>
-		</div>
-	);
+  const styles = {
+    background: "url(" + mainImage + ")",
+    borderLeft: "10px solid red",
+    position: {
+      top: "2%",
+    },
+  };
+
+  return (
+    <div
+      style={{ backgroundImage: styles.background }}
+      className="h-screen w-full bg-no-repeat bg-cover flex justify-center flex-col items-center"
+    >
+      <nav
+        className="list-none text-white text-xl md:text-2xl pt-4 flex justify-around w-full absolute"
+        style={styles.position}
+      >
+        <a
+          href="/shopping-app"
+          className="text-red-600 font-mono text-3xl font-bold"
+        >
+          Phoenix
+        </a>
+        <ul>
+          <li className="inline p-3 hover:bg-gray-700 md:mr-5 rounded active:text-red-600 cursor-pointer">
+            <a href="/shopping-app">Home</a>
+          </li>
+          <li className="inline p-3 md:mr-5 rounded hover:bg-gray-700 cursor-pointer">
+            <a href="/shop">Shop</a>
+          </li>
+          <li className="inline p-3 pr-5 rounded hover:bg-gray-700 cursor-pointer">
+            <a href="/cart">
+              <img src={cart} alt="cart" className="w-9 inline md:w-9" />
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="px-4 text-center">
+        <p
+          className="text-5xl sm:text-6xl lg:text-7xl text-white text-center p-3 font-mono"
+          style={{ borderLeft: styles.borderLeft }}
+        >
+          <span className="text-red-600 mr-3">Phoenix </span> Jewellery
+        </p>
+        <button className="text-center bg-white text-black p-3 font-serif rounded mt-6 hover:bg-gray-200 lg:text-lg">
+          Shop Now
+        </button>
+      </div>
+    </div>
+  );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,49 +4,41 @@ import mainImage from "../images/gilmore-collection.jpg";
 import { Link } from "react-router-dom";
 
 export default function Home() {
-	const styles = {
-		backgroundColor: "rgba(0, 0, 0, 0.5)",
-		background: "url(" + mainImage + ")",
-		borderLeft: "10px solid red",
-		filter: "rgba(0, 0, 0, 0.5)",
-	};
+  const styles = {
+    background: "url(" + mainImage + ")",
+    borderLeft: "10px solid red",
+  };
 
-	return (
-		<div
-			style={
-				({ backgroundImage: styles.background },
-				{ backgroundColor: styles.backgroundColor })
-			}
-			className="h-screen w-full bg-no-repeat bg-cover"
-		>
-			<nav
-				className="list-none text-white text-xl md:text-3xl flex justify-end pt-4"
-				style={{ backgroundColor: styles.filter }}
-			>
-				<li className="inline p-3 hover:bg-gray-700 md:mr-5 rounded active:text-red-600 cursor-pointer">
-					<Link to="/">Home</Link>
-				</li>
-				<li className="inline p-3 md:mr-5 rounded hover:bg-gray-700 cursor-pointer">
-					<Link to="/shop">Shop</Link>
-				</li>
-				<li className="inline p-3 pr-5 md:mr-64 rounded hover:bg-gray-700 cursor-pointer">
-					<Link to="/cart">
-						<img src={cart} alt="cart" className="w-9 inline md:w-12" />
-					</Link>
-				</li>
-			</nav>
+  return (
+    <div
+      style={{ backgroundImage: styles.background }}
+      className="h-screen w-full bg-no-repeat bg-cover"
+    >
+      <nav className="list-none text-white text-xl md:text-3xl flex justify-end pt-4">
+        <li className="inline p-3 hover:bg-gray-700 md:mr-5 rounded active:text-red-600 cursor-pointer">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="inline p-3 md:mr-5 rounded hover:bg-gray-700 cursor-pointer">
+          <Link to="/shop">Shop</Link>
+        </li>
+        <li className="inline p-3 pr-5 md:mr-64 rounded hover:bg-gray-700 cursor-pointer">
+          <Link to="/cart">
+            <img src={cart} alt="cart" className="w-9 inline md:w-12" />
+          </Link>
+        </li>
+      </nav>
 
-			<div className="flex justify-center items-center h-screen w-screen px-4 flex-col">
-				<p
-					className="text-5xl sm:text-6xl lg:text-7xl text-white break-words text-center p-3 font-mono"
-					style={{ borderLeft: styles.borderLeft }}
-				>
-					<span className="text-red-600 mr-3">Phoenix </span> Jewellery
-				</p>
-				<button className="text-center bg-white text-black p-3 font-serif rounded mt-6 hover:bg-gray-200 lg:text-lg">
-					Shop Now
-				</button>
-			</div>
-		</div>
-	);
+      <div className="flex justify-center items-center w-screen px-4 flex-col h-full">
+        <p
+          className="text-5xl sm:text-6xl lg:text-7xl text-white break-words text-center p-3 font-mono"
+          style={{ borderLeft: styles.borderLeft }}
+        >
+          <span className="text-red-600 mr-3">Phoenix </span> Jewellery
+        </p>
+        <button className="text-center bg-white text-black p-3 font-serif rounded mt-6 hover:bg-gray-200 lg:text-lg">
+          Shop Now
+        </button>
+      </div>
+    </div>
+  );
 }
