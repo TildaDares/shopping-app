3879d89292b7e2bc56dc1dd0f01386f33da5966f
TildaDares <mathildaudufo@gmail.com>
2021-03-18 20:47:22 +0100

    finished cart component design

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,25 +4,24 @@ import { Link } from "react-router-dom";
 export default function Cart(props) {
   return (
     <div>
-      <div className="max-w-screen-sm mx-auto grid grid-cols-1 sm:grid-cols-2 gap-4 mt-24 md:gap-8 p-5">
+      <div className="max-w-screen-sm mx-auto mt-24 p-5">
         {props.cartItems.map((ring) => (
-          <Link to={`/shop/${ring.id}`} key={ring.id}>
-            <div className="transition duration-500 ease-in-out transform hover:-translate-y-4 rounded-lg shadow-lg h-full max-w-sm mx-auto">
-              <img
-                src={ring.src}
-                alt={ring.desc}
-                className={"w-full object-cover h-64 mx-auto"}
-              />
-              <p className="text-center p-4 font-mono">{ring.desc}</p>
-              <p className="text-red-600 text-center pb-5">{ring.price}</p>
-              <button
-                className="p-3 bg-red-600 rounded float-right mb-4 text-white mr-3 hover:bg-red-700"
-                onClick={() => props.removeFromCart(ring)}
-              >
-                Remove from Cart
-              </button>
-            </div>
-          </Link>
+          <div>
+            <Link to={`/shop/${ring.id}`} key={ring.id}>
+              <div className="h-24 flex flex-row items-center cursor-pointer rounded-lg shadow h-full max-w-md mx-auto mb-5">
+                <div
+                  style={{ backgroundImage: `url(${ring.src})` }}
+                  className="w-24 h-24 bg-cover bg-center rounded-l-lg"
+                  title={ring.desc}
+                ></div>
+                <div className="w-full self-center">
+                  <p className="text-center p-4 pb-2 font-mono">{ring.desc}</p>
+                  <p className="text-red-600 text-center">${ring.price}</p>
+                </div>
+              </div>
+            </Link>
+            {/* <button>x</button> */}
+          </div>
         ))}
       </div>
       <button></button>
